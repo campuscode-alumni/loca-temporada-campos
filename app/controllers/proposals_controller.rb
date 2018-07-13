7c8970f133c45f66fbc4baaf0c637b114145b616
Rafael Evangelista e Carla Crude <carlacrude@gmail.com>
2018-07-12 21:36:03 -0300

    [WIP] - Analisar propostas - sem autenticação

---
+++ app/controllers/proposals_controller.rb
@@ -0,0 +1,9 @@
+class ProposalsController < ApplicationController
+  
+  def index  
+    @proposals = Proposal.all
+    if @proposals.empty?
+      flash[:alert] = 'Não existem propostas cadastradas'   
+    end
+  end
+end
