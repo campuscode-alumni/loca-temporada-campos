dac435c2b5eb96c585e1981af97732f754092218
luiz e murilo <lg.oliveira.lopes@gmail.com>
2018-07-13 21:27:06 -0300

    Login como corretor

--- spec/features/realtor_login_spec.rb
+++ spec/features/realtor_login_spec.rb
@@ -16,5 +16,18 @@ feature 'realtor login' do
     expect(page).to have_content('Signed in successfully.')
   end
 
+  scenario 'do not log in with invalid email' do
+    realtor = Realtor.create(email: 'corretor@corretora.com', password: '123123')
+
+    visit root_path
+    click_on 'Entrar como corretor'
+
+    fill_in 'Email', with: realtor.email
+    fill_in 'Senha', with: '2222' 
+    click_on 'Acessar'
+
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
 
 end
