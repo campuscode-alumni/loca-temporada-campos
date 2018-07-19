a547849c93e62e9c3bd6154c7037d5f1f78d769a
rafael e luiz <lg.oliveira.lopes@gmail.com>
2018-07-19 19:56:22 -0300

    Corrigindo login de usuário e corretor

--- spec/features/realtor/realtor_login_spec.rb
+++ spec/features/realtor/realtor_login_spec.rb
@@ -46,4 +46,17 @@ feature 'realtor login' do
     expect(page).not_to have_content(realtor.email)
     expect(page).to have_content('Login como corretor')
   end
+
+  scenario 'do not log in like user in the same session' do
+    realtor = Realtor.create(email: 'corretor@corretora.com', password: '123123')
+
+    visit root_path
+    click_on 'Login como corretor'
+
+    fill_in 'Email', with: realtor.email
+    fill_in 'Senha', with: realtor.password
+    click_on 'Acessar'
+
+    expect(page).not_to have_link('Login como usuário')
+  end
 end
--- spec/features/user/user_login_spec.rb
+++ spec/features/user/user_login_spec.rb
@@ -12,6 +12,8 @@ feature 'user login' do
     click_on 'Acessar'
 
     expect(page).to have_content('Login efetuado com sucesso.')
+    expect(page).to have_content(user.email)
+    expect(page).to have_link('Sair')
   end
 
   scenario 'do not log in with invalid email' do
@@ -25,6 +27,35 @@ feature 'user login' do
     click_on 'Acessar'
 
     expect(page).to have_content('Invalido Email ou senha')
+  end
+
+  scenario 'do not log in with realtor in the same session' do
+    user = User.create(email: 'testeimersao@gmail.com', password:'123456', cpf: '14688032390')
+
+    visit root_path
+    click_on 'Login como usuário'
+    
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Acessar'
+
+    expect(page).not_to have_link('Login como corretor')
+  end
+
+  scenario 'user logout' do
+    user = User.create(email: 'testeimersao@gmail.com', password:'123456', cpf: '14688032390')
+
+    visit root_path
+    click_on 'Login como usuário'
+    
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Acessar'
+
+    click_on 'Sair'
+
+    expect(page).not_to have_content(user.email)
+    expect(page).to have_content('Login como usuário')
   end
 
 end
